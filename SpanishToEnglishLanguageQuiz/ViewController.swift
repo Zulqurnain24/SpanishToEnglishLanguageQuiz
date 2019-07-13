083dead9af9e1a4ccaea3646f174b2ca5348df1e
Mohammad Zulqurnain <mohammad.zulqurnain@gmail.com>
2019-07-13 12:52:25 +0500

    Initial Commit

---
+++ SpanishToEnglishLanguageQuiz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SpanishToEnglishLanguageQuiz
+//
+//  Created by Mohammad Zulqarnain on 13/07/2019.
+//  Copyright © 2019 Mohammad Zulqarnain. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
